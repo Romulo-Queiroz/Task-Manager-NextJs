28b79cc688435e5171d7dbfa9023be3f29936b01
Romulo Freitas <rfcontatosvia@gmail.com>
2023-12-19 16:52:26 -0300

    Refactor:  adicionando pasta tasks. Cada componente em Pages terá sua própria pasta.

--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
-import Layout from "../components/Layout"
-import List from "../components/List"
-import Navbar from "../components/Navbar"
+import Layout from "../../components/Layout"
+import List from "../../components/List"
+import Navbar from "../../components/Navbar"
 
 const ListTasks = () => {
     
